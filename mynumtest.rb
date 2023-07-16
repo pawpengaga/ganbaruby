ffb100b5fb713742e65f761beef64fb389403fd9
Erick Rivera <yomellamoerick.771@gmail.com>
2023-07-16 07:23:31 -0400

    day 1

---
+++ mynumtest.rb
@@ -0,0 +1,24 @@
+class Numeric #LA CLASE NUMERIC SE HA EXPANDIDO EXITOSAMENTE
+    def sumar(x)
+        self+(x)
+    end
+    def restar(x)
+        self-(x)
+    end
+    def multi(x)
+        self*(x)
+    end
+    def divi(x)
+        self/(x)
+    end
+end
+
+puts 5.sumar(3)
+#####################
+puts 5.restar(3)
+#####################
+puts 5.multi(3)
+#####################
+puts 5.divi(3)
+
+
